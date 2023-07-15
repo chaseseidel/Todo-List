6c0da5ca59b5c5e65be697e471b19be91856777b
chaseseidel <chaseseidel16@gmail.com>
2023-07-14 18:14:50 -0700

    Add task and project classes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import './css/style.css';
 
-const element = document.createElement('div');
-element.textContent = 'hi';
-element.classList.add('hi');
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
 
-document.body.appendChild(element);
+    complete() {
+        this.priority = 0;
+    }
+
+    changePriority(level) {
+        this.priority = level;
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks;
+    }
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    removeTask(index) {
+        delete this.tasks[index];
+    }
+
+    getTask(name) {
+        return this.tasks[index];
+    }
+}
