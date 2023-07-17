74813e0634e966c0833e9e3a70b3445418009b80
chaseseidel <chaseseidel16@gmail.com>
2023-07-17 13:11:33 -0700

    Create task.js module

---
+++ src/modules/task.js
@@ -0,0 +1,40 @@
+export default class Task {
+    constructor(title, description, date, priority) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDate() {
+        return this.date;
+    }
+
+    setDate(date) {
+        this.date = date;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+}
