05b696f346b741ba5d82955d4e1cea6e052839e5
chaseseidel <chaseseidel16@gmail.com>
2023-07-18 15:11:21 -0700

    Create addTask function that toggles task modal

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -8,6 +8,7 @@ export default class UI {
         const ToDoList = new List();
 
         UI.Project(ToDoList);
+        UI.addTask();
     }
 
     static Project(list) {
@@ -17,7 +18,7 @@ export default class UI {
 
     static addProject() {
         const addProject = document.getElementById('add-project');
-        const closeBtn = document.querySelector('.close-btn');
+        const closeBtn = document.getElementById('project-close');
         const overlay = document.getElementById('project-overlay');
 
         addProject.addEventListener('click', () => {
@@ -33,28 +34,14 @@ export default class UI {
         });
     }
 
-    static createProjectElement(list) {
-        const submitBtn = document.getElementById('submit-btn');
-
-        submitBtn.addEventListener('click', () => {
-            DOM.createProject();
-            DOM.toggleProjectModal();
-
-            const deleteBtn = document.getElementById('user-projects').lastChild.lastChild;
-            deleteBtn.addEventListener('click', () => {
-                list.removeProject()
-            })
-        })
-    } 
-
     static createProject(list) {
-        const submitBtn = document.getElementById('submit-btn');
+        const submitBtn = document.getElementById('project-submit');
 
         submitBtn.addEventListener('click', () => {
             const name = document.getElementById('name');
             const project = new Project(name.value, list.getLength());
             list.addProject(project);
-
+        
             DOM.createProject();
             DOM.toggleProjectModal();
 
@@ -63,6 +50,29 @@ export default class UI {
                 list.removeProject(project.getID);
                 deleteBtn.parentNode.parentNode.removeChild(deleteBtn.parentNode);
             })
+
+            const thisProject = document.getElementById('user-projects').lastChild;
+            thisProject.addEventListener('click', () => {
+                console.log(project.getID);
+            })
         })
+    }
+
+    static addTask() {
+        const addTask = document.getElementById('add-task');
+        const closeBtn = document.getElementById('task-close');
+        const overlay = document.getElementById('task-overlay');
+
+        addTask.addEventListener('click', () => {
+            DOM.toggleTaskModal();
+        });
+
+        closeBtn.addEventListener('click', () => {
+            DOM.toggleTaskModal();
+        });
+
+        overlay.addEventListener('click', () => {
+            DOM.toggleTaskModal();
+        });
     }
 }
