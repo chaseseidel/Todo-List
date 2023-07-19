6571aa3a5b2f8fbdfd9c288a6166a352114a9b0e
chaseseidel <chaseseidel16@gmail.com>
2023-07-18 21:28:37 -0700

    Create current project attribute

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,10 +1,19 @@
 export default class List {
     constructor () {
         this.projects = [];
+        this.current = 0;
     }
 
     getLength() {
         return this.projects.length;
+    }
+
+    set setCurrentProject(index) {
+        this.current = index;
+    }
+
+    get getCurrentProject() {
+        return this.current;
     }
 
     addProject(project) {
@@ -13,6 +22,10 @@ export default class List {
 
     removeProject(index) {
         delete this.projects[index];
+    }
+
+    getProject(index) {
+        return this.projects[index];
     }
 
     getLength() {
