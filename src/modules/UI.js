aa4770ec8b1a2849e62d136e5e3be0e76706a39a
chaseseidel <chaseseidel16@gmail.com>
2023-07-19 10:37:44 -0700

    Fix edit submit button

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -9,6 +9,15 @@ export default class UI {
 
         UI.Project(ToDoList);
         UI.addTask();
+        UI.createTask(ToDoList);
+        UI.editTask(ToDoList);
+
+        const today = document.getElementById('default-projects');
+        today.addEventListener('click', () => {
+            console.log(ToDoList);
+        })
+
+        // UI.FixTask(ToDoList);
     }
 
     static Project(list) {
@@ -41,6 +50,7 @@ export default class UI {
             const name = document.getElementById('name');
             const project = new Project(name.value, list.getLength());
             list.addProject(project);
+            list.setCurrentProject = list.getLength() - 1;
         
             DOM.createProject();
             DOM.toggleProjectModal();
@@ -53,7 +63,9 @@ export default class UI {
 
             const thisProject = document.getElementById('user-projects').lastChild;
             thisProject.addEventListener('click', () => {
-                console.log(project.getID);
+                list.setCurrentProject = project.getID;
+                DOM.removeTasks();
+                UI.loadTasks(project);
             })
         })
     }
@@ -74,5 +86,113 @@ export default class UI {
         overlay.addEventListener('click', () => {
             DOM.toggleTaskModal();
         });
+    }
+
+    static createTask(list) {
+        const submitBtn = document.getElementById('task-submit');
+
+        submitBtn.addEventListener('click', () => {
+            const title = document.getElementById('title');
+            const description = document.getElementById('description');
+            const date = document.getElementById('date');
+            const priority = document.getElementById('priority');
+
+            const project = list.getProject(list.getCurrentProject);
+            const task = new Task(title.value, description.value, date.value, priority.value, project.getLength());
+            project.addTask(task);
+
+            DOM.toggleTaskModal();
+            DOM.createTask(title.value, date.value, priority.value, false);
+
+            UI.addIconListeners(project, task);
+        })
+    }
+
+    static loadTasks(project) {
+        const tasks = project.getTasks;
+
+        if (project.getLength() === 0) {
+            return;
+        }
+
+        tasks.forEach(task => {
+            DOM.createTask(task.getTitle, task.getDate, task.getPriority, task.getCompletion);
+            UI.addIconListeners(project, task);
+        })
+    }
+
+    static editTask(list) {
+        const submitBtn = document.getElementById('edit-submit');
+        const closeBtn = document.getElementById('edit-close');
+        const overlay = document.getElementById('edit-overlay');
+
+        const formTitle = document.getElementById('taskTitle');
+        const formDescription = document.getElementById('taskDescription');
+        const formDate = document.getElementById('taskDate');
+        const formPriority = document.getElementById('taskPriority');
+
+        submitBtn.addEventListener('click', () => {
+            const currentProject = list.getProject(list.getCurrentProject);
+            const currentTaskIndex = currentProject.getCurrent;
+            const currentTask = currentProject.getTask(currentTaskIndex);
+                        
+            currentTask.setTitle = formTitle.value;
+            currentTask.setDescription = formDescription.value;
+            currentTask.setDate = formDate.value;
+            currentTask.setPriority = formPriority.value;
+
+            DOM.removeTasks();
+            UI.loadTasks(currentProject);
+
+            DOM.toggleEditModal();
+        })
+
+        closeBtn.addEventListener('click', () => {
+            DOM.toggleEditModal();
+        });
+
+        overlay.addEventListener('click', () => {
+            DOM.toggleEditModal();
+        });
+    }
+    
+    static addIconListeners(project, task) {
+        const tasks = document.getElementById('tasks');
+        const currentTask = tasks.lastChild;
+
+        const edit = currentTask.querySelector('.edit');
+        const complete = currentTask.querySelector('.complete');
+        const remove = currentTask.querySelector('.task-delete');
+
+        const formTitle = document.getElementById('taskTitle');
+        const formDescription = document.getElementById('taskDescription');
+        const formDate = document.getElementById('taskDate');
+        const formPriority = document.getElementById('taskPriority');
+        
+        edit.addEventListener('click', () => {
+            DOM.toggleEditModal();
+
+            formTitle.value = task.getTitle;
+            formDescription.value = task.getDescription;
+            formDate.value = task.getDate;
+            formPriority.value = task.getPriority;
+
+            project.setCurrent = task.getID;
+        })
+
+        complete.addEventListener('click', () => {
+            if (task.completed === false) {
+                task.completeTask();
+                currentTask.className = 'task complete-task';
+            } else {
+                task.undoCompletion();
+                currentTask.className = `task ${task.getPriority}`;
+            }
+        })
+
+        remove.addEventListener('click', () => {
+            project.removeTask(task.getID)
+            tasks.removeChild(currentTask);
+        })
     }
 }
