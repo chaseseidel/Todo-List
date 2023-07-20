4d3e61e69177418f2e97799312e3b49d1c22dbd1
chaseseidel <chaseseidel16@gmail.com>
2023-07-20 15:13:42 -0700

    Fix final functions

--- src/index.js
+++ src/index.js
@@ -1,53 +1,6 @@
 import './css/style.css';
+import UI from './modules/UI';
+import DOM from './modules/elements';
 
-class Task {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-
-    complete() {
-        this.priority = 0;
-    }
-
-    changePriority(level) {
-        this.priority = level;
-    }
-}
-
-class Project {
-    constructor(name) {
-        this.name = name;
-        this.tasks = [];
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    setName(name) {
-        this.name = name;
-    }
-
-    getTasks() {
-        return this.tasks;
-    }
-
-    setTasks(tasks) {
-        this.tasks = tasks;
-    }
-
-    addTask(task) {
-        this.tasks.push(task);
-    }
-
-    removeTask(index) {
-        delete this.tasks[index];
-    }
-
-    getTask(name) {
-        return this.tasks[index];
-    }
-}
+DOM.loadHomePage();
+UI.loadUI();
